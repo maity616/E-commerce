a54b868c3c0e6ff7dbff00f02c4b292a85726a6c
Sudip Maity <maitysudip616@gmail.om>
2025-05-12 16:23:54 +0530

    first commit update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
 import { Router, Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
-import Home from './components/Home';
-import Jewelery from './components/Jewelery';
-import Menfashin from './components/Menfashin';
-import Womenfashin from './components/Womenfashin';
-import Electronic from './components/Electronic';
-import Cart from './components/Cart';
-import SingleProduct from './components/SingleProduct';
+
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRout';
 import Layout from './components/layout';
@@ -25,7 +17,7 @@ function App() {
       <Route element={<Login />} path='/login' />
       
         <Route element={<ProtectedRoute>
-          <Layout /> </ProtectedRoute>} path='/' />
+          <Layout /> </ProtectedRoute>} path='*' />
        
        
       </Routes>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,5 @@
 import React from 'react'
 import { Router, Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
-// import Home from './components/Home';
-// import Jewelery from './components/Jewelery';
-// import Menfashin from './components/Menfashin';
-// import Womenfashin from './components/Womenfashin';
-// import Electronic from './components/Electronic';
-// import Cart from './components/Cart';
-// import SingleProduct from './components/SingleProduct';
-// import Login from './components/Login';
-// import ProtectedRoute from './components/ProtectedRout';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
@@ -20,27 +11,22 @@ import Cart from './Cart';
 import SingleProduct from './SingleProduct';
 const Layout = () => {
   return (
-   <>
-   
-   <Header/>
-     
-     <Routes>
-       
-    
-     
-       <Route element={
-         <Home/> } path='/' />
-      
-       <Route path="/product/:id" element={<SingleProduct />} />
-       <Route element={<Jewelery />} path='/jewelery' />
-       <Route element={<Menfashin />} path='/menfashin' />
-       <Route element={<Womenfashin />} path='/womenfashin' />
-       <Route element={<Electronic />} path='/electronic' />
-       <Route element={<Cart />} path='/cart' />
-     </Routes>
-    
-     <Footer/>
-   </>
+    <>
+
+      <Header />
+      <Routes>
+        <Route element={
+        <Home />} path='*' />
+        <Route path="/product/:id" element={<SingleProduct />} />
+        <Route element={<Jewelery />} path='/jewelery' />
+        <Route element={<Menfashin />} path='/menfashin' />
+        <Route element={<Womenfashin />} path='/womenfashin' />
+        <Route element={<Electronic />} path='/electronic' />
+        <Route element={<Cart />} path='/cart' />
+      </Routes>
+
+      <Footer />
+    </>
   )
 }
 
